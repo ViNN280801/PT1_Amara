f82e8436fedb949e30df5afd4e2ab20d27fd431c
Vladislav Semykin <vladislav_semykin01@mail.ru>
2022-11-29 09:57:47 +0300

    Created structure of main

--- main.c
+++ main.c
@@ -1,9 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <malloc.h>
+#include "menu_funcs.h"
+
+void print_menu()
+{
+    int menuVar = 0;
+    printf("Введите номер задания (от 1 до 5; 0 - для выхода): ");
+    menuVar = inputInt();
+
+    while (1)
+    {
+        if (menuVar == 0)
+        {
+            printf("Выход...\n");
+            exit(0);
+        }
+
+        switch (menuVar)
+        {
+        case 1:
+        {
+            printf("Введите номер задания (от 1 до 5; 0 - для выхода): ");
+            menuVar = inputInt();
+            break;
+        }
+        case 2:
+        {
+            printf("Введите номер задания (от 1 до 5; 0 - для выхода): ");
+            menuVar = inputInt();
+            break;
+        }
+        case 3:
+        {
+            printf("Введите номер задания (от 1 до 5; 0 - для выхода): ");
+            menuVar = inputInt();
+            break;
+        }
+        case 4:
+        {
+            printf("Введите номер задания (от 1 до 5; 0 - для выхода): ");
+            menuVar = inputInt();
+            break;
+        }
+        case 5:
+        {
+            printf("Введите номер задания (от 1 до 5; 0 - для выхода): ");
+            menuVar = inputInt();
+            break;
+        }
+        default:
+        {
+            printf("Введён неверный номер задания. Попробуйте снова: ");
+            menuVar = inputInt();
+            break;
+        }
+        }
+    }
+}
 
 int main()
 {
+    // Установка русского языка
+    setlocale(LC_ALL, "rus");
+
+    // Запуск меню
+    print_menu();
+
     return 0;
 }
